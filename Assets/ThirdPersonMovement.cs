9268dc48fb85cb9631e165e28798291c4deeedc6
Ethan L <ethanlanting@gmail.com>
2023-02-07 08:00:05 -0600

    chnages

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -7,6 +7,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
     public CharacterController controller;
     public float speed = 6f;
+    public float runSpeed = 10f;
     public float turnSmoothTime = 0.1f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
@@ -27,7 +28,14 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * speed * Time.deltaTime);
+
+
+            // If the player is holding shift, use runSpeed
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                controller.Move(moveDir * runSpeed * Time.deltaTime);
+            } else {
+                controller.Move(moveDir * speed * Time.deltaTime);
+            }
         }
 
         if (controller.isGrounded && Input.GetButtonDown("Jump"))
